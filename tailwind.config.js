70dd61138225cf046d538cc30a6b750f28101317
tuaemtramtinh <anh.nguyen2252038@hcmut.edu.vn>
2024-03-21 19:39:54 +0700

    FIRST

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+tailwind.config = {
+    theme: {
+			screens: {
+				'sm': '576px',
+				'md': '768px',
+				'lg': '992px',
+				'xl': '1200px',
+				'2xl': '1200px'
+			},
+			container: {
+				center: true,
+				screens: {
+					'sm': '576px',
+					'md': '768px',
+					'lg': '992px',
+					'xl': '1200px',
+					'2xl': '1200px',
+				},
+			},
+    }
+}
